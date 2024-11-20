a7cd1102c653051f206478fc46466bee42556af6
Caleb Stripes <calebdstripes@gmail.com>
2024-11-20 10:27:17 -0500

    updated test for better coverage

---
+++ tests/hiros/test_hiros.py
@@ -0,0 +1,30 @@
+import unittest
+
+from hiros.hiro_class import HiroClass, AbilityScores, SkillList
+
+class TestHiroClass(unittest.TestCase):
+    def setUp(self):
+        self.hiro = HiroClass('Hiro', AbilityScores(10,10,10,10,10,10))
+    def tearDown(self):
+        return super().tearDown()
+    
+    def test_HiroClass_init(self):
+        self.assertEqual(self.hiro.name, 'Hiro')
+        self.assertEqual(self.hiro.character_level, 1)
+        self.assertEqual(self.hiro.proficiency_bonus, 2)
+        
+        self.assertEqual(self.hiro.ability_scores.strength, 10)
+        self.assertEqual(self.hiro.ability_scores.dexterity, 10)
+        self.assertEqual(self.hiro.ability_scores.constitution, 10)
+        self.assertEqual(self.hiro.ability_scores.intelligence, 10)
+        self.assertEqual(self.hiro.ability_scores.wisdom, 10)
+        self.assertEqual(self.hiro.ability_scores.charisma, 10)
+        
+        self.assertEqual(self.hiro.ability_modifiers.get('STR'), 0)
+        self.assertEqual(self.hiro.ability_modifiers.get('DEX'), 0)
+        self.assertEqual(self.hiro.ability_modifiers.get('CON'), 0)
+        self.assertEqual(self.hiro.ability_modifiers.get('INT'), 0)
+        self.assertEqual(self.hiro.ability_modifiers.get('WIS'), 0)
+        self.assertEqual(self.hiro.ability_modifiers.get('CHA'), 0)
+        
+        self.assertIsInstance(self.hiro.skill_list, SkillList)
