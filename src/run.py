cefd56736dbda78a7a0f3d55ae5e5cffe5749c64
Caleb Stripes <calebdstripes@gmail.com>
2024-11-21 15:19:11 -0500

    setup for experimentation

--- src/run.py
+++ src/run.py
@@ -3,32 +3,20 @@ from rolls.roll_table import RollTable, RtItem
 from rolls.roller import Roller
 
 def main():
+    einar = HiroClass('Einar', AbilityScores(15,16,12,8,10,14))
+    roll_table = RollTable()
+    roll_table.add_item('Potion of Healing', 'Heals 5 hp', weight=5)
+    roll_table.add_rt_item(RtItem('Common Sword', 'Deals 1d6 slashing damage', weight=10))
+    roll_table.add_rt_item(RtItem('Common Dagger', 'Deals 1d4 piercing damage', weight=10))
+    roll_table.add_rt_item(RtItem('Arcane Hand Cannon', 'Deals 1d10 fire damage', weight=1))
     
-    ######## HIRO ########
-    # scores = AbilityScores(15,10,13,8,12,14)
-    # einar = HiroClass('Einar', scores)
-    # smite = Roller(character=einar, roll_type='standard', ability='CHA', logging=True, dice=['3d8', '1d10'])
-    # climb = Roller(character=einar, roll_type='attack', ability='STR', skill='Athletics')
-    # import pdb; pdb.set_trace()
     
-    ######## ROLL TABLE ########
-    rt = RollTable('loot')
-    potion = RtItem('Potion of Healing', 'Heals 5 hp', weight=5)
-    coin = RtItem('Coin', '1 cp', weight=10)
-    sword = RtItem('Sword', '1d6 damage', weight=5)
-    magical_sword = RtItem('Magical Sword', '1d8 damage', weight=1)
-    rt.add_rt_item(potion)
-    rt.add_rt_item(coin)    
-    rt.add_rt_item(sword)
-    rt.add_rt_item(magical_sword)
-    print(rt.table_data)
-    for item in rt.table_data[1:]:
-        print(item.define())
+    print('dir(einar):', dir(einar))
+    print('')
+    print('dir(roll_table):', dir(roll_table))
+    print('')
+    import pdb; pdb.set_trace()
 
-    i=10
-    while i > 0:
-        print(rt.roll().define())
-        i -= 1
 if __name__ == "__main__":
     main()
     
